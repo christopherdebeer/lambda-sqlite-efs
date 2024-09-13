921ddaeb363b859bde80b759325a94c26826fbb6
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-09-13 16:42:48 +0200

    fix: use env variables

--- bin/lambda-sqlite-efs.ts
+++ bin/lambda-sqlite-efs.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { LambdaSqliteEfsStack } from '../lib/lambda-sqlite-efs-stack';
+import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
+import { LambdaSqliteEfsStack } from "../lib/lambda-sqlite-efs-stack";
 
 const app = new cdk.App();
-new LambdaSqliteEfsStack(app, 'LambdaSqliteEfsStack', {
+new LambdaSqliteEfsStack(app, "LambdaSqliteEfsStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,10 @@ new LambdaSqliteEfsStack(app, 'LambdaSqliteEfsStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: {
+    account: process.env.AWS_ACCOUNT_NUMBER,
+    region: process.env.AWS_REGION,
+  },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
