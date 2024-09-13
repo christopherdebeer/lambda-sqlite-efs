9977ab4b2852f61ad13b274d644c3f5b5776f50f
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-09-13 16:37:54 +0200

    cdk init

---
+++ lib/lambda-sqlite-efs-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import * as efs from 'aws-cdk-lib/aws-efs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+export class LambdaSqliteEfsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new Vpc(this, "api-vpc", {});
+    // The code that defines your stack goes here
+
+    const EFS_MOUNT_PATH = "/mnt/sqlite";
+
+    const dataBaseEFS = new efs.FileSystem(this, "db-file-system", {
+      vpc,
+    });
+
+    const accessPoint = dataBaseEFS.addAccessPoint("EfsAccessPoint", {
+      createAcl: {
+      ownerGid: "1001",
+      ownerUid: "1001",
+      permissions: "750"
+    },
+      path: "/lambda",
+      posixUser: {
+        gid: "1001",
+        uid: "1001"
+      }
+    });
+
+    const api = new lambda.Function(this, "api-function", {
+      vpc,
+      filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, EFS_MOUNT_PATH),
+      environment: {
+        MOUNT_PATH: EFS_MOUNT_PATH,
+      },
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      code: lambda.Code.fromInline(`export const handler = async (event) => { console.log("Hello World", { event })};`)
+    });
+
+  }
+}
---
+++ test/lambda-sqlite-efs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaSqliteEfs from '../lib/lambda-sqlite-efs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-sqlite-efs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaSqliteEfs.LambdaSqliteEfsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
