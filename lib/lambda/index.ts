1ce35ffa4edb4358b4fe764d3ebebd162dd67431
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-09-19 21:25:10 +0200

    feat: try read file

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -4,5 +4,8 @@ const EFS_PATH = process.env.EFS_PATH;
 
 export const handler = (event: any) => {
   console.log("Hello World! ", {event});
-  fs.writeFileSync(EFS_PATH + "/123.txt", "1\n2\n3");
+//   fs.writeFileSync(EFS_PATH + "/123.txt", "1\n2\n3");
+    const file = fs.readFileSync(EFS_PATH + "/123.txt");
+
+    console.log(JSON.stringify({file}));
 };
