7f8f7d888a9ac9e982a963dfd1113313e33228cd
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-09-19 21:29:57 +0200

    feat: use utf8

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -1,11 +1,11 @@
-import * as fs from "fs";
+import * as fs from "node:fs";
 
 const EFS_PATH = process.env.EFS_PATH;
 
 export const handler = (event: any) => {
   console.log("Hello World! ", {event});
 //   fs.writeFileSync(EFS_PATH + "/123.txt", "1\n2\n3");
-    const file = fs.readFileSync(EFS_PATH + "/123.txt");
+    const file = fs.readFileSync(EFS_PATH + "/123.txt", "utf8");
 
     console.log(JSON.stringify({file}));
 };
